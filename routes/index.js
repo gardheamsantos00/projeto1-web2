3e4c7582f746c615f4091bc772f721aaf8c5a4d8
gardheamsantos <gardheamsantos@outlook.com>
2019-05-20 20:15:43 -0300

    cad list edit remove orientacao

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,17 @@ const mongoose = require('mongoose');
 require("../models/Orientacao");
 const Orientacao = mongoose.model("orientacoes");
 
+
+
+var erros = [];
+
 router.get('/' , (req, res)=>{
     res.render("admin/index");
     //res.send("testeaaaaaaa");
 });
 
+
+//LISTAR ORIENTAÇÃO
 router.get('/orientacoes', (req, res)=>{
 
     Orientacao.find().sort({date: 'desc'}).then( (orientacoes) => {
@@ -27,9 +33,10 @@ router.get('/orientacoes/add', (req, res)=>{
     res.render("admin/addorientacao");
 });
 
+
+//CADASTRAR ORIENTAÇÃO
 router.post('/orientacoes/nova', (req, res)=>{
     
-    var erros = [];
 
     //valida se o nome vier vazio null undefine
     if(!req.body.nome || 
@@ -63,11 +70,74 @@ router.post('/orientacoes/nova', (req, res)=>{
         });
 
     }
-    
-    
+ 
+});
+
+//EDIT ORIENTAÇÃO
+router.get("/orientacoes/edit/:id", (req,res) => {
+    Orientacao.findOne({_id:req.params.id}).then( (orientacao) =>{
+        
+        res.render("admin/editorientacao", {orientacao:orientacao});
+
+    }).catch( (error) => {
+        req.flash("error_msg", "Esta orientação não existe");
+        res.redirect("/auth/orientacoes");
+    });
 });
 
 
+//Edita a orientacao e salva 
+router.post("/orientacao/edit", (req,res)=>{
+    Orientacao.findOne({_id:req.body.id}).then( (orientacao) =>{
+
+    //valida se o nome vier vazio null undefine
+        if(!req.body.id || 
+            req.body.id == null || 
+            typeof req.body.id == undefined){
+
+            erros.push({texto: "Id inválido!"});
+        }
+        
+        if(req.body.nome.length < 2){
+            erros.push({texto : "Edição do nome deve possuir mais que 2 caracters"});
+        }
+
+        if(erros.length > 0 ){
+            res.render("admin/addorientacao", { erros : erros })
+        }else{
+            // falta as validações de campos , fazer depois!
+        orientacao.nome = req.body.nome;
+
+        orientacao.save().then( ()=> {
+            req.flash("success_msg" , "Orientação editada com sucesso");
+            res.redirect("/auth/orientacoes");
+        }).catch( (error)=>{
+            req.flash("error_msg", "Houve um erro ao salvar a edição da orientação");
+            res.redirect("/auth/orientacoes");
+            console.log("error save edit" + error);
+        });
+        
+        }
+        
+
+    }).catch( (error) => {
+        req.flash("error_msg", "Error na edição de orientacao");
+        res.redirect("/auth/orientacoes");
+        console.log("find error save edit" + error);
+    });
+})
+
+
+
+router.post("/orientacao/del" , (req,res) => {
+    Orientacao.deleteOne({_id: req.body.id}).then( () => {
+        req.flash("success_msg", "Orientacao deletada com sucesso!");
+        res.redirect("/auth/orientacoes");
+    }).catch( (error) => {
+        req.flash("error_msg", "Houve um erro ao tentar deletar a orientação");
+        res.redirect("/auth/orientacoes");
+    });
+});
 
 
 //exportar o arquivo
